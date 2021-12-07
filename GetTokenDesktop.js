f04e8d94468d9b15d3a63c9f57191eddec4999cc
Sc00pe <93505279+Sc00pe@users.noreply.github.com>
2021-12-07 20:03:14 +1100

    Fixed

--- GetTokenDesktop.js
+++ GetTokenDesktop.js
@@ -1,4 +1,4 @@
 let popup;
-popup = window.open('', '', `top=0,left=${screen.width-800},width=850,height=150`);
-token = popup.localStorage.token // GET TOKEN FROM DISCORD STORAGE
-token = token.slice(1, -1); // REMOVE QUOTATIONS
+    popup = window.open('', '', `top=0,left=${screen.width-800},width=850,height=150`);
+    token = popup.localStorage.token // GET TOKEN  FROM DISCORD STORAGE
+    token = token.slice(1, -1); // REMOVE QUOTATIONS
